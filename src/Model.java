11267aa416a3e27c1e5e488cc503064766421dc7
Eric Carlsson <cirecirecire3@gmail.com>
2019-10-03 11:00:19 +0200

    clean up

--- src/Model.java
+++ src/Model.java
@@ -1,30 +1,19 @@
 import java.util.Random;
 
-/**
- * The physics model.
- * 
- * This class is where you should implement your bouncing balls model.
- * 
- * The code has intentionally been kept as simple as possible, but if you wish, you can improve the design.
- * 
- * @author Simon Robillard
- *
- */
 public class Model {
 
 	double areaWidth, areaHeight;
 
-	final double GRAVITY = 0.0005;
-	int numberOfBalls = 500;
-	double averageSpeed = 0.01;
+	final double GRAVITY = 0.5;
+	int numberOfBalls = 20;
+	double averageSpeed = 0.5;
 	
 	Ball [] balls;
 
 	Model(double width, double height) {
 		areaWidth = width;
 		areaHeight = height;
-		
-		// Initialize the model with a few balls
+
 		balls = getBalls(numberOfBalls, averageSpeed, areaHeight, areaWidth);
 		//balls = new Ball[2];
 		//balls[0] = new Ball(width / 3, height * 0.9, 2.2, 1.6, 0.2);
@@ -45,15 +34,9 @@ public class Model {
 		return balls;
 	}
 
-	boolean collidesWithBorder (double radius, double position, double border, double velocity) {
-		if ((position < radius && velocity < 0) || (position > border - radius && velocity > 0)) {
-			return true;
-		}
-		return false;
-	}
+
 
 	void step(double deltaT) {
-		// TODO this method implements one step of simulation with a step deltaT
 		for (Ball b : balls) {
 
 			b.v.vy -= GRAVITY;
@@ -68,44 +51,32 @@ public class Model {
 			for (Ball b2 : balls){
 				if (b != b2 && b.checkBallCollsion(b2)){
 					ballCollision(b, b2);
-					//slowBall(b2);
-					//slowBall(b);
 				}
 			}
-			
-			// compute new position according to the speed of the ball
+
 			b.x += deltaT * b.v.vx;
 			b.y += deltaT * b.v.vy;
 		}
 	}
 
-	void slowBall (Ball ball){
-		ball.v = ball.v.subtract(new Vector(ball.v.vx * 0.05,ball.v.vy * 0.05));
+	boolean collidesWithBorder (double radius, double position, double border, double velocity) {
+		if ((position < radius && velocity < 0) || (position > border - radius && velocity > 0)) {
+			return true;
+		}
+		return false;
 	}
-
-
 
 	void ballCollision ( Ball b1, Ball b2){
-
 		Vector t = new Vector(b1.x - b2.x, b1.y - b2.y);
-
 		Vector relV = b1.v.subtract(b2.v);
-
 		Vector force = relV.projectOnVector(t);
-
 		b1.v = b1.v.subtract(force);
-
 		b2.v = b2.v.plus(force);
-
 	}
-	
-	/**
-	 * Simple inner class describing balls.
-	 */
+
 	class Ball {
 
 		double x, y, radius;
-
 		Vector v;
 
 		Ball(double x, double y, double vx, double vy, double r) {
@@ -114,7 +85,6 @@ public class Model {
 			this.v = new Vector(vx, vy);
 			this.radius = r;
 		}
-
 
 		boolean checkBallCollsion (Ball b2){
 			double distance = Math.sqrt(Math.pow(this.x - b2.x,2)+Math.pow(this.y - b2.y,2));
@@ -143,10 +113,8 @@ public class Model {
 
 		Vector projectOnVector (Vector tangent) {
 			double scalar = this.dotproduct(tangent) / tangent.dotproduct(tangent);
-
 			tangent.vx *= scalar;
 			tangent.vy *= scalar;
-
 			return tangent.copy();
 		}
 
